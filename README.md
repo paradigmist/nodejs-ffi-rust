6a8cca992e6a2545d505abc8beb219305918b6dc
Kane ONG <dividedbynil@gmail.com>
2018-03-06 20:18:00 +0800

    refine the program for command line argument input

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Using Rust as a NodeJS addon
 1. `git clone https://github.com/paradigmist/nodejs-ffi-rust.git`
 2. `cd nodejs-ffi-rust`
 3. `npm i && cargo build --release`
-4. `node fibs.js`
+4. `node fibs.js 35`
 
 Have fun!
--- fibs.js
+++ fibs.js
@@ -5,6 +5,13 @@ const rust = ffi.Library('target/release/' + (!isWin ? 'lib' : '') + 'ffi', {
   fib: ['int', ['int']]
 })
 
+if (process.argv.length < 3) {
+  console.log('Arguments: ' + process.argv[0] + ' ' + process.argv[1] + ' ???')
+  process.exit()
+}
+
+const input = parseInt(process.argv[2])
+
 function fib(n) {
   if (n === 1 || n === 2) {
     return 1
@@ -12,12 +19,11 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2)
 }
 
-// js
-console.time('node')
-console.log(fib(40))
-console.timeEnd('node')
+function benchmark(str, fn) {
+	console.time(str)
+	console.log(fn(input))
+	console.timeEnd(str)
+}
 
-// rust
-console.time('rust')
-console.log(rust.fib(40))
-console.timeEnd('rust')
+benchmark('node', fib)
+benchmark('rust', rust.fib)
